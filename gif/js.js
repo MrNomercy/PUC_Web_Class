3d7b2692025c5974bac27458f124f09619f9f30c
MrNomercy <43256301+MrNomercy@users.noreply.github.com>
2019-08-20 21:16:12 +1200

    Add files via upload

---
+++ gif/js.js
@@ -0,0 +1,24 @@
+document.write("hellow");
+function myfunction(){
+window.alert("hello javascript");
+//console.log("hello buddy");
+}
+document.getElementById('text').innerHTML = "hello";
+	var animal = ['cat','dog','egg','fish','goat'];
+	var l = "";
+	for (var i = 0; i < 5; i++) 
+	{
+		l = animal[i];
+	}
+	document.getElementById("go").style.backgroundColor = "blue";
+
+	function date()
+	{
+		document.getElementById("time").innerHTML = Date();	
+		document.getElementById("demo").style.fontSize = "25px";
+	}
+
+	document.write("shit");
+
+
+
